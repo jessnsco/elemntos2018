90794a92289de0cecca9e6dfb6da7ea45d9279e7
T-107 <T-107@PC199.mshome.net>
2018-05-25 21:47:49 -0500

    Problemita

---
+++ src/main/java/unitec/elementosmvc/Mensaje.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.elementosmvc;
+
+/**
+ *
+ * @author T-107
+ */
+public class Mensaje {
+    
+    @Id
+    String id;
+    
+    String cuerpo;
+
+    public Mensaje(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public Mensaje() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+}
